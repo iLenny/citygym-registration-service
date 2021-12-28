ca9bb1af08a32851e852d9753f118b81aeb07e42
Leibniz H. Berihuete <leibniz.berihuete@gmail.com>
2021-12-28 11:58:27 -0500

    Initial commit

---
+++ src/main/java/com/lennycodes/citygym/CitygymRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lennycodes.citygym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitygymRegistrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitygymRegistrationServiceApplication.class, args);
+    }
+
+}
